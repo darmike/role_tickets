94fa652cc76ea3bfde77a464e611cdba8e71a53f
darmike <darmogray777@gmail.com>
2024-08-11 17:21:06 +0300

    create routes

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('groups/', views.group_list, name='group_list'),
+    path('groups/create/', views.group_create, name='group_create'),
+    path('tickets/', views.ticket_list, name='ticket_list'),
+    path('tickets/create/', views.ticket_create, name='ticket_create'),
+    path('users/', views.user_list, name='user_list'),
 ]
