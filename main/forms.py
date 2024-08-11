f3e3da1e8b6cc57ca3075741aa1100a173a4a806
darmike <darmogray777@gmail.com>
2024-08-11 17:04:39 +0300

    add forms for group, ticket, user

---
+++ main/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Group, Ticket, User
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name', 'description']
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description', 'group']
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'role']
--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Модель користувача
+class User(AbstractUser):
+    role = models.CharField(max_length=50)
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
