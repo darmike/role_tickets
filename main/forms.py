9740911dc7e8ffd686b9c0c16c2829f5bb2699c5
darmike <darmogray777@gmail.com>
2024-08-11 17:19:43 +0300

    add form and views fot group and users

--- main/forms.py
+++ main/forms.py
@@ -1,15 +1,18 @@
 from django import forms
 from .models import Group, Ticket, User
 
+
 class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = ['name', 'description']
 
+
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'group']
+
 
 class UserForm(forms.ModelForm):
     class Meta:
--- main/views.py
+++ main/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import GroupForm, TicketForm, UserForm
+from .models import Group, Ticket, User
+
+
+@login_required
+def group_list(request):
+    groups = Group.objects.all()
+    return render(request, 'groups/groups_list.html', {'groups': groups})
+
+
+@login_required
+def group_create(request):
+    if request.method == 'POST':
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('group_list')
+    else:
+        form = GroupForm()
+    return render(request, 'groups/groups_create.html', {'form': form})
+
+
+@login_required
+def ticket_list(request):
+    tickets = Ticket.objects.all()
+    return render(request, 'tickets/tickets_list.html', {'tickets': tickets})
+
+
+@login_required
+def ticket_create(request):
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_list')
+    else:
+        form = TicketForm()
+    return render(request, 'tickets/tickets_create.html', {'form': form})
+
+
+@login_required
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'users/users_list.html', {'users': users})
